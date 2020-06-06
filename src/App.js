47fb8c103edf06382c8eb1828b5fba333dbcc512
leeeunji93 <59554635+leeeunji93@users.noreply.github.com>
2020-06-06 23:47:02 +0900

    Read Me

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import CreateInformation from './components/CreateInformation';
 import Main from './components/Main';
+import './App.scss';
 
 const App = () => {
   return (
-    <div>
+    <div className="app">
       <Route exact={true} path="/" component={Main} />
       <Route path="/createinformation" component={CreateInformation} />
     </div>
--- src/components/CreateInformation.js
+++ src/components/CreateInformation.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './CreateInformation.scss';
+import './Button.scss';
 
 // 여기서 추가 하면 어떻게 Main.js로 데이터가 넘어갈 것인가?
 const CreateInformation = ({
@@ -14,6 +16,7 @@ const CreateInformation = ({
     <div>
       <form>
         <input
+          className="title"
           name="title"
           placeholder="제목을 입력하세요"
           onChange={onChange}
@@ -21,12 +24,14 @@ const CreateInformation = ({
           ref={nameInput}
         />
         <input
+          className="date"
           name="date"
           placeholder="날짜를 입력하세요"
           onChange={onChange}
           value={date}
         />
         <input
+          className="content"
           name="content"
           placeholder="내용을 입력하세요"
           onChange={onChange}
@@ -34,7 +39,9 @@ const CreateInformation = ({
         />
       </form>
       <Link to="/">
-        <button onClick={onCreate}>추가</button>
+        <button className="button" onClick={onCreate}>
+          추가
+        </button>
       </Link>
     </div>
   );
